d059ce990206328db0b529544fa4d101ae4b8fd8
Sima Maniyeva <83311302+Simaniyeva@users.noreply.github.com>
2026-02-14 00:04:08 +0400

    Happy Number solution

--- 202. Happy Number.py
+++ 202. Happy Number.py
@@ -1,12 +1,16 @@
-n=int(input())
-digits=set()
-while n!=1 and n not in digits:
-    digits.add(n)
-    sum=0
-    while n>0:
-        n,digit=divmod(n,10)
-        sum+=digit*digit
-    n=sum
-return n==1
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        digits=set()
+        while n!=1 and n not in digits:
+            digits.add(n)
+            sum=0
+            while n>0:
+                n,digit=divmod(n,10)
+                sum+=digit*digit
+            n=sum
+        return n==1
         
-    
